49065545ec7bac9483f7efc2207721757ff88367
Manuel Santiago Cabeza <msantiagoc33@gmail.com>
2023-05-14 20:08:09 +0200

    modificacion

--- src/fechas/Fechas.java
+++ src/fechas/Fechas.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * subido a github 14/5/23
  */
 package fechas;
 
@@ -28,7 +29,7 @@ public class Fechas {
         // imprimir el numero de dias entre dos fecha
         LocalDate fechaNacimiento = LocalDate.of(1957, 6, 9);
         long dias = ChronoUnit.YEARS.between(fechaNacimiento,fechaActual);
-        System.out.println("Años entre dos fechas: "+dias);
+        System.out.println("Aï¿½os entre dos fechas: "+dias);
         
         // imprimir el mes de una fecha
         System.out.println("El mes de mi nacimiento es "+fechaNacimiento.getMonth());
@@ -39,14 +40,14 @@ public class Fechas {
         // aumentar en tres meses la fecha actual
         System.out.println("La fecha dentro de 3 meses es "+fechaActual.plusMonths(3));
         
-        // aumentar la fecha actual 1 año y 2 meses
-        System.out.println("La fecha dentro de 1 año y 3 meses es "+fechaActual.plusYears(1).plusMonths(2));
+        // aumentar la fecha actual 1 aï¿½o y 2 meses
+        System.out.println("La fecha dentro de 1 aï¿½o y 3 meses es "+fechaActual.plusYears(1).plusMonths(2));
         
         // fecha 10 dias antes de hoy
         System.out.println("Fecha 10 dias antes de la fecha actual: "+fechaActual.minusDays(10));
 
-        // representa el primer dia del presente año
-        System.out.println("El primer dia del presente año era "+fechaActual.withDayOfYear(1));
+        // representa el primer dia del presente aï¿½o
+        System.out.println("El primer dia del presente aï¿½o era "+fechaActual.withDayOfYear(1));
         
         // ultimo dia del presente mes
         System.out.println("El ultimo dia del presente mes es "+fechaActual.with(TemporalAdjusters.lastDayOfMonth()));
